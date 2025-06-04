166a2766abb409649f7018f263e13e4125cdc462
TERlYAKI <louvelrouz13@gmail.com>
2025-06-04 08:16:30 +0800

    first commit

---
+++ src/main/java/com/mycompany/javaguigroupoop/JavaGUIGroupOOP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaguigroupoop;
+
+/**
+ *
+ * @author Louvel
+ */
+public class JavaGUIGroupOOP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
