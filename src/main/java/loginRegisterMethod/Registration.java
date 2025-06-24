b788906914033a4a8b437fd55e1d934f94c41553
TERlYAKI <louvelrouz13@gmail.com>
2025-06-24 16:30:19 +0800

    Improved the Authentications

--- src/main/java/loginRegisterMethod/Registration.java
+++ src/main/java/loginRegisterMethod/Registration.java
@@ -18,12 +18,27 @@ public class Registration {
     public static ArrayList<String> password = new ArrayList<>();
     
     public static void registerUser(String uname, String uemail, String upass){
-        name.add(uname);
-        email.add(uemail);
-        password.add(upass);
+        if(isRegistered(uname, uemail, upass) == false) {
+            name.add(uname);
+            email.add(uemail);
+            password.add(upass);
+            System.out.println("Registered: " + uname + ", " + uemail + ", " + upass);
+            JOptionPane.showMessageDialog(null, "Registered Successfully");
+        }else{
+            JOptionPane.showMessageDialog(null,"Account Already Exist");
+        } 
     }
     
-   public static void findUser(String uemail, String upass){
+    public static boolean isRegistered(String uname, String uemail, String upass){
+        for(int i = 0; i < name.size(); i++){
+           if(uname.equals(name.get(i))&& uemail.equals(email.get(i)) && (upass.equals(password.get(i)))){
+                return true;
+           }         
+       }
+        return false;
+    }
+    
+    public static void findUser(String uemail, String upass){
        for(int i = 0; i < name.size(); i++){
            if(uemail.equals(email.get(i)) && (upass.equals(password.get(i)))){
                JOptionPane.showMessageDialog(null, "Welcome\n" + name.get(i));
